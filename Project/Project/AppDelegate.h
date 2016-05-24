4d31741a11fac225d25cdf9750fa3a87c56353ef
your name <your@email.com>
2016-05-24 10:03:06 +0800

    初始化项目完成

---
+++ Project/Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by 石伟 on 16/5/24.
+//  Copyright © 2016年 石伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
